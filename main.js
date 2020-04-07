20b205509f5a210656f969ac12673f94add41667
Dave.L <59891341+Ciph4r@users.noreply.github.com>
2020-04-07 16:27:31 -0400

    'done'

--- main.js
+++ main.js
@@ -1,7 +1,43 @@
 /******************
  * YOUR CODE HERE *
  ******************/
-
+const newPerson = (firstName ,lastName , age, married = false ) => {
+  // if (firstName === undefined)
+  //  {firstName = 'Anonymous'}
+  let newPer = {firstName,
+                lastName,
+                age,
+                married,
+                goingOn: function(){
+                    return this.age +1
+                  },
+                ageUp: function(){
+                    return this.age += 1
+                  },
+                getFullName: function(){
+                    return this.firstName + ' ' + this.lastName
+                  },
+                marry: function(obj){
+                    this.married = true
+                    obj.married = true
+                    this.spouseName = obj.getFullName()
+                    obj.spouseName = this.getFullName()
+                  },
+                divorce: function(obj){
+                    this.married = false
+                    obj.married = false
+                    delete this.spouseName
+                    delete obj.spouseName
+                  },
+                }
+                
+                
+                if (firstName === undefined){
+                        newPer.firstName = 'Anonymous'
+                        newPer.lastName = 'Person'
+                    }
+    return newPer
+}
 
 
 /*********************************
